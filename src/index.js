0648eb17eb43f37084c550f17281709ad3fc68d0
Sergey Gorin <seygorin@gmail.com>
2023-02-21 18:29:02 +0500

    feat: complete!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  return
-}
+module.exports = function reverse(n) {
+  const b = Math.abs(n);
+  const a = b.toString();
+  const splitString = a.split('');
+  const reverseArray = splitString.reverse();
+  const joinArray = reverseArray.join('');
+  
+  return joinArray;
+};
